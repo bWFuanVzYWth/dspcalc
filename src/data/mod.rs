5ee135fb268b7f408e66ae6692abea89398dd956
bWFuanVzYWth <origin_0110@outlook.com>
2025-02-25 04:53:26 +0800

    求解框架

--- src/data/mod.rs
+++ src/data/mod.rs
@@ -1,1 +1,3 @@
 pub mod dsp;
+
+// TODO 不同游戏的item枚举，封装成通用类型
--- src/solver/mod.rs
+++ src/solver/mod.rs
@@ -1,7 +1,11 @@
 use std::collections::HashMap;
 
 use crate::data::dsp::item::Item;
-use good_lp::{variable, ProblemVariables, Variable};
+use good_lp::{
+    clarabel,
+    solvers::clarabel::{self, ClarabelProblem},
+    variable, Expression, ProblemVariables, SolverModel, Variable,
+};
 use strum::IntoEnumIterator;
 
 #[derive(Clone, Eq, PartialEq, Hash)]
@@ -10,7 +14,14 @@ struct Cargo {
     point: u64, // 增产点数
 }
 
-fn tmp() {
+// 计算当前决策的成本
+fn cost(vars: &HashMap<&Cargo, Variable>) -> Expression {
+    // TODO 多种预设加权
+    vars.values().sum()
+}
+
+// TODO 传入需求、约束
+fn tmp() -> Result<clarabel::ClarabelSolution, good_lp::solvers::ResolutionError> {
     // 根据需要求解的问题，创建包含了所有相关物品的扁平化列表
     let cargos = Item::iter()
         .map(|item| {
@@ -27,10 +38,15 @@ fn tmp() {
 
     // 在求解器中声明物品，并返回物品在求解器中的引用
     let mut problem = ProblemVariables::new();
-    let hashmap = cargos
+    let vars = cargos
         .iter()
         .map(|cargo| (cargo, problem.add(variable().name(cargo.item.to_string()))))
         .collect::<HashMap<_, _>>();
 
-    //
+    // 给问题建模
+    let mut model = problem.minimise(cost(&vars)).using(clarabel);
+
+    // TODO 根据合成公式创建所有约束
+
+    model.solve()
 }
