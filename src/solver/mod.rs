387f84e434fc9171a7709dd8813025fcb0bf65ad
bWFuanVzYWth <origin_0110@outlook.com>
2025-03-09 01:45:30 +0800

    优化求解精度

--- src/solver/mod.rs
+++ src/solver/mod.rs
@@ -237,9 +237,13 @@ pub fn solve() {
     problem
         .settings()
         .verbose(true) // 启用详细输出
-        .tol_gap_abs(f64::EPSILON) // 绝对对偶间隙容差
-        .tol_gap_rel(f64::EPSILON) // 相对对偶间隙容差
-        .tol_feas(f64::EPSILON) // 可行性容差
+        .tol_gap_abs(f64::EPSILON)
+        .tol_gap_rel(f64::EPSILON)
+        .tol_feas(f64::EPSILON)
+        .tol_infeas_abs(f64::EPSILON)
+        .tol_infeas_rel(f64::EPSILON)
+        .static_regularization_constant(f64::EPSILON)
+        .dynamic_regularization_eps(f64::EPSILON)
         .max_iter(u32::MAX); // 最大迭代次数
 
     constraint_recipe(
