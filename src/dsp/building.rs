297d76d7d9d4f7db059d5783d5f4bce517e343e1
bWFuanVzYWth <origin_0110@outlook.com>
2025-03-13 13:44:24 +0800

    初步修复了重氢引入的计算错误

--- src/dsp/building.rs
+++ src/dsp/building.rs
@@ -7,6 +7,7 @@ pub enum BuildingType {
     精炼厂 = 3,
     制造台 = 4,
     对撞机 = 5,
+    分馏塔 = 8,
     科研站 = 15,
     矿机,
     喷涂机,
@@ -15,15 +16,16 @@ pub enum BuildingType {
 
 impl BuildingType {
     #[must_use]
-    pub const fn from_recipe_item(recipe_item: &RecipeItem) -> Self {
+    pub fn from_recipe_item(recipe_item: &RecipeItem) -> Self {
         match recipe_item.type_ {
             1 => Self::熔炉,
             2 => Self::化工,
             3 => Self::精炼厂,
             4 => Self::制造台,
             5 => Self::对撞机,
+            8 => Self::分馏塔,
             15 => Self::科研站,
-            _ => panic!("Fatal: unknown building_type"),
+            _ => panic!("Fatal: unknown building_type: {}", recipe_item.type_),
         }
     }
 
@@ -35,6 +37,7 @@ impl BuildingType {
             Self::精炼厂 => 1.0,
             Self::制造台 => 1.0 / 3.0,
             Self::对撞机 => 1.0,
+            Self::分馏塔 => 1.0,
             Self::科研站 => 1.0 / 3.0,
             Self::矿机 => 1.0,
             Self::喷涂机 => 1.0,
@@ -50,6 +53,7 @@ impl BuildingType {
             Self::精炼厂 => 960.0,
             Self::制造台 => 2700.0,
             Self::对撞机 => 12000.0,
+            Self::分馏塔 => 17800.0, // FIXME 这个值是错误的，或者说这个公式本来就是错误的。只是为了规范而保留，任意时候都不应该使用
             Self::科研站 => 1920.0,
             Self::矿机 => 25100.0,
             Self::喷涂机 => 90.0,
@@ -57,17 +61,17 @@ impl BuildingType {
         }
     }
 
-    pub const fn area(&self) -> f64 {
-        match self {
-            Self::熔炉 => 2880.0,
-            Self::化工 => 2160.0,
-            Self::精炼厂 => 960.0,
-            Self::制造台 => 2700.0,
-            Self::对撞机 => 12000.0,
-            Self::科研站 => 1920.0,
-            Self::矿机 => 25100.0,
-            Self::喷涂机 => 90.0,
-            Self::小太阳 => 288_000.0,
-        }
-    }
+    // pub const fn area(&self) -> f64 {
+    //     match self {
+    //         Self::熔炉 => 2880.0,
+    //         Self::化工 => 2160.0,
+    //         Self::精炼厂 => 960.0,
+    //         Self::制造台 => 2700.0,
+    //         Self::对撞机 => 12000.0,
+    //         Self::科研站 => 1920.0,
+    //         Self::矿机 => 25100.0,
+    //         Self::喷涂机 => 90.0,
+    //         Self::小太阳 => 288_000.0,
+    //     }
+    // }
 }
