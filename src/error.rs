b7db73fa818dc84697329717e10369f69b626320
bWFuanVzYWth <origin_0110@outlook.com>
2025-03-19 02:00:06 +0800

    简化报错

--- src/error.rs
+++ src/error.rs
@@ -2,7 +2,7 @@ use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum DspCalError {
-    #[error("mismatched recipe weights: {{recipes count:{0}, weights count:{0}}}")]
+    #[error("mismatched recipe weights: recipes count:{0}, weights count:{0}")]
     MismatchedRecipeWeights(usize, usize),
     #[error("lp solver error: {0}")]
     LpSolverError(good_lp::ResolutionError),
